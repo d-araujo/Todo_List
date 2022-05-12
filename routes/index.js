4a0b8abf91e038b42b74231d6dd6b197a560e0f8
Maria Eduarda de Araujo <84139806+d-araujo@users.noreply.github.com>
2022-05-12 09:34:10 -0300

    12/05/2022 - LP

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 module.exports = (app)=>{
 
     //rota aleatória
-    app.get('/',(req,res)=>{    
-    res.send("qualquer coisa")
+    app.get('/',(req,res)=>{   
+    //res.send('qualquer coisa') 
+    res.render('index.ejs')
     })
 }
